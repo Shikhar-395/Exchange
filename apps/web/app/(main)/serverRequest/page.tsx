a077a11a6b33b7a0a829a48c589e49d07e972c84
shikhar-395 <shikharpersonal8@gmail.com>
2026-03-05 11:43:21 +0530

    corrected server reqeust page

--- apps/web/app/(main)/serverRequest/page.tsx
+++ apps/web/app/(main)/serverRequest/page.tsx
@@ -3,14 +3,17 @@ import { cookies } from "next/headers";
 
 export default async function Page() {
   const cookie = await cookies();
-  const fetchTodos = async () => {
-    const todo = await axios.get("http://localhost:3001/api/v1/todos", {
-      headers: {
-        cookie: cookie.toString(),
-      },
-    });
-    console.log(todo.data);
-  };
-  await fetchTodos();
-  return <div>hi</div>;
+  const todo = await axios.get("http://localhost:3001/api/v1/todos", {
+    headers: {
+      cookie: cookie.toString(),
+    },
+  });
+  console.log(todo.data.todo.todos[0].todo);
+  return (
+    <div>
+      hi
+      {/*@ts-ignore*/}
+      {todo.data.todo.todos[0].todo}
+    </div>
+  );
 }
